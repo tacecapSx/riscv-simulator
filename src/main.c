afbcbaced920d4f580529fbc650f23782fea6fac
tacecapSx <martin.handest@gmail.com>
2023-11-06 15:47:16 +0100

    collecting .bin data into 32-bit words

--- src/main.c
+++ src/main.c
@@ -11,34 +11,55 @@ const uint32_t x0 = 0;
 //register x1 to x32
 uint32_t x[31];
 
-//memory with 2³² Gb RAM.
-uint32_t mem[4294967295];
+//memory with 1 mb RAM.
+uint32_t mem[262144];
+
+long get_file_size(FILE* file) {
+    fseek(file, 0L, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+
+    return file_size;
+}
 
 //Function to load program
 int load(char* fname){
-    FILE* openF = fopen(fname, "r");
+    printf("Opening .bin file...\n");
+
+    FILE* file = fopen(fname, "r");
     int i = 0;
-    while (1) {
-        int read = fgetc(openF);
-        if (feof(openF)) {
-            break;
+
+    long file_size = get_file_size(file);
+
+    printf("Collecting binary data into program memory...\n");
+
+    while (ftell(file) < file_size - 4) {
+        // collecting chars into one 32-bit instruction
+        uint32_t read = 0;
+        for(int j = 3; j >= 0; j--) {
+            uint32_t byte = fgetc(file);
+
+            read = (byte << (j * 8)) | read;
         }
-        printf("%x", read);
+
+        printf("%x ", read);
         mem[i] = read;
         i++;
     }
-    printf("\n");
+
+    printf("\nReplicating collected data...\n");
+
     for (int j = 0; j < i; j++) {
-        printf("%x", mem[j]);
+        printf("%x ", mem[j]);
     }
-    fclose(openF);
+    fclose(file);
     return 0;
 }
 
 //Function to read instruction from memory
-int readInstr() {
-    uint32_t instr = mem[pc];
-    pc ++;
+int read_instruction() {
+    uint32_t instruction = mem[pc];
+    pc++;
 }
 
 
